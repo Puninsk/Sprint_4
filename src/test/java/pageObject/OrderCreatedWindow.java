377dddaf54bfb33673d0d837b29d3f587969e2fb
Nikita Puninskiy <puninsk@yandex.ru>
2023-02-27 00:06:39 +0300

    Second commit

---
+++ src/test/java/pageObject/OrderCreatedWindow.java
@@ -0,0 +1,24 @@
+package pageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class OrderCreatedWindow {
+
+    private WebDriver driver;
+
+    public OrderCreatedWindow(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    private By OrderCreatedWindow = By.xpath("//*[@class='Order_Modal__YZ-d3']/*[text()='Заказ оформлен']");
+
+    public void waitForOrderConfirmationButton() {
+        new WebDriverWait(driver, 3).until(ExpectedConditions.visibilityOfElementLocated(OrderCreatedWindow));
+    }
+
+}
+
+
---
+++ src/test/java/tests/FaqSectionTest.java
@@ -0,0 +1,76 @@
+package tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pageObject.Faq;
+import pageObject.TestData;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class FaqSectionTest {
+    private WebDriver driver;
+
+    private final String expectedQuesitonTextFaq;
+
+    private final String locatorFaqQuestion;
+
+    private final String questionToOpen;
+
+    static TestData testData = new TestData();
+
+    public FaqSectionTest( String questionToOpen, String locatorFaqQuestion, String expectedQuesitonTextFaq) {
+        this.questionToOpen = questionToOpen;
+        this.locatorFaqQuestion = locatorFaqQuestion;
+        this.expectedQuesitonTextFaq = expectedQuesitonTextFaq;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getQuestionText() {
+        return new Object[][]{
+                {testData.firstQuestionTitleLocator, testData.firstQuestionTextLocator, testData.firstQuestionText},
+                {testData.secondQuestionTitleLocator, testData.secondQuestionTextLocator, testData.secondQuestionText},
+                {testData.thirdQuestionTitleLocator, testData.thirdQuestionTextLocator, testData.thirdQuestionText},
+                {testData.forthQuestionTitleLocator, testData.forthQuestionTextLocator, testData.forthQuestionText},
+                {testData.fifthQuestionTitleLocator, testData.fifthQuestionTextLocator, testData.fifthQuestionText},
+                {testData.sixthQuestionTitleLocator, testData.sixthQuestionTextLocator, testData.sixthQuestionText},
+                {testData.seventhQuestionTitleLocator, testData.seventhQuestionTextLocator, testData.seventhQuestionText},
+                {testData.eightsQuestionTitleLocator, testData.eightsQuestionTextLocator, testData.eightsQuestionText},
+
+        };
+    }
+
+    @Before
+    public void setUpBrowser() {
+        driver = new ChromeDriver();
+    }
+    @Test
+    public void FaqSectionTest() {
+
+
+        driver.get(testData.urlSamokat);
+
+        Faq objFaq = new Faq(driver);
+
+        objFaq.findFaqSection();
+
+        objFaq.acceptCookieClick();
+
+        String actual = objFaq.getQuestionText(questionToOpen, locatorFaqQuestion);
+
+        String expected = expectedQuesitonTextFaq;
+
+        assertEquals(expected, actual);
+
+    }
+
+    @After
+    public void cleanUpImportantWindows() {
+        driver.quit();
+    }
+}
